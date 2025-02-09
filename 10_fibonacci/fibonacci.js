073b0eb4530edcd2cda0d3a0dd0e2f5c29a6cd7e
OlaTheEast <easton.rayner@gmail.com>
2025-02-09 13:48:42 -0600

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,39 @@
-const fibonacci = function() {
+function recursive_fibonacciSequenceBuilder(index) {
+	if (index == 0 || index == 1) {
+		return index;
+	}
 
+	const previousNumber = recursive_fibonacciSequenceBuilder(index - 1);
+	const secondPreviousNumber = recursive_fibonacciSequenceBuilder(index - 2);
+	const currentNumber = previousNumber + secondPreviousNumber;
+	return currentNumber;
+}
+
+function iterative_fibonacciSequenceBuilder(index) {
+	let secondPreviousNumber = 0;
+	let previousNumber = 1;
+	let currentNumber = 0;
+	if (index == 0 || index == 1) {
+		return index;
+	}
+
+	for (let i = 1; i < index; i++) {
+		currentNumber = secondPreviousNumber + previousNumber;
+		[secondPreviousNumber, previousNumber] = [previousNumber, currentNumber];
+	}
+
+	return currentNumber;
+}
+
+const fibonacci = function (number) {
+	if (typeof number == "string") {
+		number = parseInt(number);
+	}
+
+	if (number < 0) {
+		return "OOPS";
+	}
+	return recursive_fibonacciSequenceBuilder(number);
 };
 
 // Do not edit below this line
