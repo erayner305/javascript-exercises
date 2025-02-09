c28455397074a1eab97c05aab4fc22fb845c67c2
OlaTheEast <easton.rayner@gmail.com>
2025-02-09 14:10:26 -0600

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getPersonAge(person) {
+    let yearOfBirth = person.yearOfBirth;
+    let yearOfDeath = person.yearOfDeath || new Date().getFullYear();
 
+	return yearOfDeath - yearOfBirth;
+}
+
+const findTheOldest = function (people) {
+	let oldest = people.reduce((oldestPerson, currentPerson) => {
+		const oldestPersonAge = getPersonAge(oldestPerson);
+		const currentPersonAge = getPersonAge(currentPerson);
+		if (currentPersonAge <= oldestPersonAge) {
+			return oldestPerson;
+		}
+
+		return currentPerson;
+	});
+	console.log(oldest);
+	return oldest;
 };
 
 // Do not edit below this line
