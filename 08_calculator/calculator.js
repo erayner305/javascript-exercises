630eeecb040373c66df1369b3d9f2ce0094fa31f
OlaTheEast <easton.rayner@gmail.com>
2025-02-09 13:04:37 -0600

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+	return numbers.reduce((total, currentNumber) => {
+		return total + currentNumber;
+	}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+	return numbers.reduce((total, currentNumber) => {
+		return total * currentNumber;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let result = 1;
+	for (let i = num; i > 1; i--) {
+		result *= i;
+	}
+
+	return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
